cd6675e9dda297a2345fdcf6187771d56e95c712
FrancoDev <aaronfranco17@gmail.com>
2017-09-13 13:51:35 -0500

    Hooked up Database

--- Eventure/Eventure/settings.py
+++ Eventure/Eventure/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'Eventure.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bifrost_brogram',
+        'USER': 'bifrost_brogram',
+        'PASSWORD': 'YkLD6Gfa3UcRM6KR',
+        'HOST': 'easel1.fulgentcorp.com',
+        'PORT': '3306',
     }
 }
 
