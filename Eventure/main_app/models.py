25f2457efba7c73b8eb99c3f3bfd143e0d405389
Hector Flores <hectorflores19z@gmail.com>
2017-10-02 13:50:45 -0500

    Model changes

--- Eventure/main_app/models.py
+++ Eventure/main_app/models.py
@@ -18,7 +18,7 @@ class UserProfile(models.Model):
         return self.firstName + ' ' + self.lastName
 
 class EventInfo(models.Model):
-    id = models.CharField(primary_key = True, max_length = 10, default = '')
+    id = models.CharField(primary_key = True, max_length = 12, default = '')
     userProfile = models.ForeignKey(UserProfile, null = True)
     type = models.BooleanField(default = False)  #auto-set to public
     name = models.CharField(max_length = 255, default = '')
@@ -58,10 +58,10 @@ class Attendee(models.Model):
         
     )
     
-    name = models.CharField(max_length=256, default = '')
-    attendeeID = models.CharField(max_length = 6, default = '')
-    userattendeeID = models.IntegerField(default = -1)
+    attendeeName = models.CharField(max_length=256, default = '')
+    attendeeID = models.CharField(max_length = 8, default = '')
+    userAttendeeID = models.IntegerField(null = True)
     eventID = models.ForeignKey(EventInfo, null = True)
     itemID = models.ForeignKey(Item, null = True)
     email = models.EmailField(max_length=256, default='')
-    RSVP = models.IntegerField(choices=RSVPSTATUS, blank=True, null=True)
+    RSVPStatus = models.IntegerField(choices=RSVPSTATUS, blank=True, null=True)
