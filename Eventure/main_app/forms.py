527653795c61c2380ef7f23a4b793f84b3e5f961
Hector Flores <hectorflores19z@gmail.com>
2017-11-02 21:32:13 -0500

    Fixed type that was causing django error

--- Eventure/main_app/forms.py
+++ Eventure/main_app/forms.py
@@ -41,7 +41,7 @@ class itemMForm(forms.ModelForm):
 class takeItemForm(forms.ModelForm):
 	class Meta:
 		model = TakenItem
-		fields = ('itemBeingBroughtID', 'attendeeID','itemLinkID','eventId','quantity','comment',)
+		fields = ('itemBeingBroughtID', 'attendeeID','itemLinkID','eventID','quantity','comment',)
 		
 class CreateEventForm(forms.ModelForm):
 
