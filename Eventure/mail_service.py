6228070a25a44ce4004827bcde5cd55220507b6f
Eduardo J. Mata <juarez.e12@gmail.com>
2017-11-27 15:47:29 -0600

    Pushing last changes i made, it is not fully working

--- Eventure/mail_service.py
+++ Eventure/mail_service.py
@@ -1,20 +1,40 @@
 import smtplib
-from django.template import loader, RequestContext
+from django.template import loader, RequestContext, Context
 from django.core.mail import EmailMessage, EmailMultiAlternatives
+from django.core import mail
 from django.utils.html import strip_tags
+import html2text
 
 #global variable for recipient list
 recipientList =[]
+emaiLinkDict = {}
+items = []
 
-def create_recipient_list(email):
+
+def create_recipient_list(email, emailLink):
    recipientList.append(email)
+   emaiLinkDict.update({email:emailLink})
 
-def sendEmailToAtendees(eventName, host, request):
-   subject = '{host} has invited you to {eventName}'.format(host = host, eventName=eventName)
-   html_message = loader.render_to_string('displayEvent.html')
-   text_content = "This is the text_content"
-   msg = EmailMultiAlternatives(subject, html_message, recipientList)
-   msg.content_subtype = "html"
-   msg.send()
-   print (subject,recipientList)
+def itemsPerEvent(item):
+   items.append(item)
+
+def sendEmailToAtendees(eventObject, newEmailInvitee , host, request):
+    connection = mail.get_connection()
+    connection.open()
+    subject = '{host} has invited you to {eventName}'.format(host = host, eventName=eventObject.name)
+    content = {'eventInfo':eventObject,
+               'items':items,
+               'guests': newEmailInvitee}
+    for attendeeEmail in recipientList:
+        email = EmailMessage(subject,emaiLinkDict.get(attendeeEmail), recipientList)
+        email.send()
+
+    connection.close()
+    print (subject,recipientList, content)
+    #text_content = "This is the Text content"
+    #html_message = loader.render_to_string('displayEvent.html', content)
+    #print(text_content)
+    #msg = EmailMultiAlternatives(subject, html_message, recipientList)
+    #msg.content_subtype = "html"
+    #smsg.send()
 
